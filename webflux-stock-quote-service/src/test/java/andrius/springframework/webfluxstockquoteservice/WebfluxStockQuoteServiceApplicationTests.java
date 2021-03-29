b6cd8b3c93a3b66eb86784b0662fa1fad0a593db
andntp <andntp@corp.rst.lt>
2021-03-29 15:20:12 +0300

    Innit

---
+++ webflux-stock-quote-service/src/test/java/andrius/springframework/webfluxstockquoteservice/WebfluxStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package andrius.springframework.webfluxstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
